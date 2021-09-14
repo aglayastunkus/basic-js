9b340891608914bb59b2ba628bd1a868f25c84ce
aglaya <aglayastunkus1@gmail.com>
2021-09-14 20:53:10 +0300

    feat: implemented countCats and dateSample functions

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,7 +14,9 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]) => 3`
  *
  */
-export default function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function countCats(matrix) {
+    return matrix
+        .map(item => item
+            .reduce((count, num) => num === '^^' ? count + 1 : count, 0))
+        .reduce((count, num) => count + num, 0)
 }
